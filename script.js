cf581b0c6612c8146a70aceffd28522f70a82c19
SAJDI-HABIB <sajidchughtai779@gmail.com>
2025-11-08 18:47:32 +0500

    resolved conflicts

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let borad = document.querySelector("#chessbord");
 
 //unicode chhess pieces 
@@ -62,3 +63,84 @@ function handleclick (square) {
         selectedSquare = null;
     }
 }
+=======
+let studentName = document.querySelector("#student");
+let english = document.querySelector("#english");
+let math = document.querySelector("#math");
+let  computer = document.querySelector("#computer");
+let calculate = document.querySelector("#calculate");
+let result = document.querySelector("#result");
+
+let total = 0;
+let percentage = 0;
+let staus;
+
+calculate.addEventListener("click", function () {
+    let Name = studentName.value;
+    let engli = Number(english.value);
+    let mat = Number(math.value);
+    let com = Number(computer.value)
+
+    if (Name === "" && engli === "" && mat == "" && com === "") {
+    result.textContent = `please fill all field`;
+}
+
+total = engli + mat + com;
+
+percentage = total / 3;
+staus = (percentage >= 40) ? "pass" : "your are fail";
+
+let grade = "";
+
+if (percentage >= 80) {
+    grade = "A";
+} else if (percentage >= 60) {
+    grade = "B"
+} else if (percentage >= 50) {
+    grade = "C";
+} else if (percentage >= 40) {
+    grade = "d"
+} else 
+    grade = "F"
+    
+
+//final out put
+
+result.innerHTML = `
+<h3>Result for :${Name}</h>
+total marks : ${total}<br>
+percentage : ${percentage.toFixed(2)}% <br>
+grade : ${grade}
+status : ${staus}
+`;
+
+});
+
+
+
+
+
+
+//  calculate.addEventListener ("click", function () {
+
+
+
+//     total = english + math + computer ;
+//     percentage = total / 3;
+//     if (percentage > 40 ) {
+//         percentage.textContent = `congrulation you have pass`;
+//     } else if (percentage < 40) {
+//         percentage.textContent = `oh so sad you are fail`
+//     }
+//     if (percentage >= 80) {
+//         result.textContent = `ypur grade is A`
+//     } else if (percentage >= 60) {
+//         result.textContent = `your grade is B`;
+//     } else if (percentage >= 50) {
+//         result.textContent = `your garde  c`
+//     }
+//     result();
+//  }); 
+
+// calculate();
+>>>>>>> f5dcc5f (Initial commit)
